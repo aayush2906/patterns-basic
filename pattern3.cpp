5a3ee8b7a6c5cdb650bf25845300d1550ed036f9
Aayush Diwan <aayushdiwan10@gmail.com>
2019-01-15 22:22:45 +0530

    Create pattern3.cpp
    *****
    ****
    ***
    **
    *

---
+++ pattern3.cpp
@@ -0,0 +1,23 @@
+/*
+*****
+****
+***
+**
+*
+*/
+main()
+{  //patern 3
+	for(int i=1;i<6;i++)
+	{
+		for(int j=1;j<6;j++)
+		{
+			if(j<=6-i)
+			cout<<"*";
+			else
+			cout<<" ";
+		}
+		cout<<"\n";
+	}
+	
+}
+
