5a14b7a208147fc899d5d3ae4e78a40f86b97b41
Aayush Diwan <aayushdiwan10@gmail.com>
2019-01-16 08:28:08 +0530

    Create pattern5.py
    pattern 5
        *    
       ***   
      *****  
     ******* 
    *********

---
+++ pattern5.py
@@ -0,0 +1,17 @@
+for i in range(1,6):
+    for j in range(1,10):
+        if j>=6-i and j<=4+i:
+            print("*",end="");
+        else:
+            print(" ",end="");
+    print();
+
+'''
+pattern 5
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+
+'''
