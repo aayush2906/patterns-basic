fae4ed49360efa63b1b831abe6d4a84f5c7a5c89
Aayush Diwan <aayushdiwan10@gmail.com>
2019-01-16 08:21:13 +0530

    Create pattern4.py
    pattern4
        *
       **
      ***
     ****
    *****

---
+++ pattern4.py
@@ -0,0 +1,16 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if j>=6-i:
+            print("*",end="");
+        else:
+            print(" ",end="");
+    print();
+
+'''
+pattern4
+    *
+   **
+  ***
+ ****
+*****
+'''
