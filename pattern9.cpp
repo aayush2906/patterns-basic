31553a4b2711e7aeff95cf72c1029be0aa94e86a
Aayush Diwan <aayushdiwan10@gmail.com>
2019-01-21 23:57:27 +0530

    Create pattern9.cpp
    ABCDCBA
    ABC  CBA
    AB      BA
    A          A

---
+++ pattern9.cpp
@@ -0,0 +1,29 @@
+ /*
+ 
+ABCDCBA
+ABC CBA
+AB   BA
+A     A
+*/
+//pattern 9
+main()
+
+{
+char k;
+
+for(int i=1;i<=4;i++)
+	{
+		k='A';
+	for(int j=1;j<=7;j++)
+	{
+		if(j<=5-i|| j>=3+i)
+		{
+		cout<<k;
+		j<4?k++:k--;}
+		else
+		cout<<" ";
+		
+	}
+	cout<<"\n";
+}
+}
